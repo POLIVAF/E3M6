2f393c2925dc3cb689017c90558e5ac7d4a97cb9
POLIVAF <reikikapablo@gmail.com>
2026-02-09 22:52:59 -0300

    se agrega carpeta view y archivo perfil, ademasse configura motor de plantilla y ruta dinamica en app

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const PORT = 3000;
 // Middleware para archivos estáticos
 app.use(express.static('public'));
 
+// Configurar motor de plantillas
+app.set('view engine', 'hbs');
+
+// Ruta dinámica
+app.get('/perfil', (req, res) => {
+  res.render('perfil', {
+    nombre: 'Ana',
+    profesion: 'Desarrolladora Web'
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
